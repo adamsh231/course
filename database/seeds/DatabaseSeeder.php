3ce0bbd1764febb98559d4703e46aa05b9fca378
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-03-07 21:07:58 +0700

    Create Migration and Seeder for Siswa

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Faker::create('id_ID');
+        for ($i = 1; $i <= 20; $i++) {
+            DB::table('siswa')->insert([
+                'id' => $i,
+                'name' => $faker->name,
+                'username' => $faker->userName,
+                'password' => bcrypt('dayung231'),
+                'email' => $faker->email,
+                'phone' => $faker->phoneNumber,
+                'pretest'=> $faker->numberBetween(0,100),
+                'posttest'=> $faker->numberBetween(0,100),
+                'team'=> $faker->numberBetween(1,8),
+            ]);
+        }
     }
 }
