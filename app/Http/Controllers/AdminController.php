338af98f36f104a4c7afbaa72d404a6617156e35
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-03-14 13:52:40 +0700

    Adding Admin Function Pertemuan (Kuis, File, Kegiatan)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Siswa;
 use App\Pertemuan;
+use App\Detail;
+use App\File;
+use App\Kuis;
+use App\Soal;
+use App\Presensi;
 
 class AdminController extends Controller
 {
@@ -12,11 +17,26 @@ class AdminController extends Controller
     {
         $siswa = $this->getAllSiswa();
         $pertemuan = $this->getAllPertemuan();
+        $detail = [];
+        foreach ($pertemuan as $p) {
+            $detail[$p->id] = $this->getDetailByPertemuan($p->id);
+        }
+        $file = [];
+        foreach ($pertemuan as $p) {
+            $file[$p->id] = $this->getFileByPertemuan($p->id);
+        }
+        $kuis = [];
+        foreach ($pertemuan as $p) {
+            $kuis[$p->id] = $this->getKuisByPertemuan($p->id);
+        }
         return view(
             'admin',
             [
                 'siswa' => $siswa,
-                'pertemuan' => $pertemuan
+                'pertemuan' => $pertemuan,
+                'detail' => $detail,
+                'file' => $file,
+                'kuis' => $kuis
             ]
         );
     }
@@ -31,5 +51,22 @@ class AdminController extends Controller
     {
         $pertemuan = Pertemuan::all();
         return $pertemuan;
+    }
+
+    function getDetailByPertemuan($key)
+    {
+        $detail = Detail::where('id_pertemuan', $key)->get();
+        return $detail;
+    }
+
+    function getFileByPertemuan($key)
+    {
+        $file = File::where('id_pertemuan', $key)->get();
+        return $file;
+    }
+
+    function getKuisByPertemuan($key){
+        $kuis = Kuis::where('id_pertemuan', $key)->get();
+        return $kuis;
     }
 }
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,15 +4,17 @@
 @section('content')
 <div class="content-body">
     <div class="container-fluid">
+
+        {{-- CARD1 --}}
         <div class="card">
             <div class="card-body">
                 <div class="default-tab">
                     <ul class="nav nav-tabs mb-3" role="tablist">
                         <li class="nav-item">
-                            <a class="nav-link active show" data-toggle="tab" href="#siswa">Siswa</a>
+                            <a class="nav-link active show" onclick="info_pertemuan(0)" data-toggle="tab" href="#siswa">Siswa</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="#pertemuan">Pertemuan</a>
+                            <a class="nav-link" onclick="info_pertemuan(1)" data-toggle="tab" href="#pertemuan">Pertemuan</a>
                         </li>
                     </ul>
                     <div class="tab-content">
@@ -86,7 +88,7 @@
                                         @foreach ($pertemuan as $p)
                                         <tr>
                                             <td class="text-center">{{ $loop->iteration }}</td>
-                                            <td>{{ $p->nama }}</td>
+                                            <td onclick="info_kegiatan({{ $loop->index }})"><a href="#kegiatan">{{ $p->nama }}</a></td>
                                             <td>{{ $p->judul }}</td>
                                             <td class="text-center">{{ date('d/m/Y', strtotime($p->tanggal)) }}</td>
                                             <td class="text-center">
@@ -130,6 +132,202 @@
                 </div>
             </div>
         </div>
+        {{-- END CARD1 --}}
+
+        {{-- CARD2 --}}
+        <div id="pertemuan-detail" class="card d-none">
+            <div class="card-body">
+                <div class="default-tab">
+                    <ul class="nav nav-tabs mb-3" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active show" data-toggle="tab" href="#kegiatan">Kegiatan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#file">File</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#kuis">Kuis</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane fade active show" id="kegiatan" role="tabpanel">
+                            @foreach ($pertemuan as $p)
+                            <div class="table-responsive check-kegiatan d-none">
+                                <div class="card-title text-center">
+                                    <h3>{{ ucwords($p->nama) }}</h3>
+                                </div>
+                                <table class="table table-bordered table-striped verticle-middle">
+                                    <thead>
+                                        <tr class="text-center">
+                                            <th scope="col">No</th>
+                                            <th scope="col">Kegiatan</th>
+                                            <th scope="col">Deskripsi</th>
+                                            <th scope="col">Mulai</th>
+                                            <th scope="col">Selesai</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($detail[$p->id] as $d)
+                                        <tr>
+                                            <td class="text-center">{{ $loop->iteration }}</td>
+                                            <td>{{ $d->kegiatan }}</td>
+                                            <td>{{ $d->deskripsi }}</td>
+                                            <td>{{ $d->mulai }}</td>
+                                            <td>{{ $d->selesai }}</td>
+                                            <td class="text-center">
+                                                <span>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit Data">
+                                                        <i class="fa fa-pencil color-muted m-r-5"></i>
+                                                    </a>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete Data">
+                                                        <i class="fa fa-close color-danger"></i>
+                                                    </a>
+                                                </span>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            @endforeach
+                        </div>
+                        <div class="tab-pane fade" id="file" role="tabpanel">
+                            @foreach ($pertemuan as $p)
+                            <div class="table-responsive check-file d-none">
+                                <div class="card-title text-center">
+                                    <h3>{{ ucwords($p->nama) }}</h3>
+                                </div>
+                                <table class="table table-bordered table-striped verticle-middle">
+                                    <thead>
+                                        <tr class="text-center">
+                                            <th scope="col">No</th>
+                                            <th scope="col">Nama</th>
+                                            <th scope="col">Deskripsi</th>
+                                            <th scope="col">File</th>
+                                            <th scope="col">Jenis</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($file[$p->id] as $f)
+                                        <tr>
+                                            <td class="text-center">{{ $loop->iteration }}</td>
+                                            <td>{{ $f->nama }}</td>
+                                            <td>{{ $f->deskripsi }}</td>
+                                            <td class="text-center">
+                                                <span>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Change File">
+                                                        <i class="fa fa-refresh color-muted m-r-5"></i>
+                                                    </a>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Open File">
+                                                        <i class="fa fa-eye color-danger"></i>
+                                                    </a>
+                                                </span>
+                                            </td>
+                                            <td>{{ $f->jenis }}</td>
+                                            <td class="text-center">
+                                                <span>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit Data">
+                                                        <i class="fa fa-pencil color-muted m-r-5"></i>
+                                                    </a>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete Data">
+                                                        <i class="fa fa-close color-danger"></i>
+                                                    </a>
+                                                </span>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            @endforeach
+                        </div>
+                        <div class="tab-pane fade" id="kuis" role="tabpanel">
+                            @foreach ($pertemuan as $p)
+                            <div class="table-responsive check-kuis d-none">
+                                <div class="card-title text-center">
+                                    <h3>{{ ucwords($p->nama) }}</h3>
+                                </div>
+                                <table class="table table-bordered table-striped verticle-middle">
+                                    <thead>
+                                        <tr class="text-center">
+                                            <th scope="col">No</th>
+                                            <th scope="col">Nama</th>
+                                            <th scope="col">Jawaban</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($kuis[$p->id] as $k)
+                                        <tr>
+                                            <td class="text-center">{{ $loop->iteration }}</td>
+                                            <td>{{ $k->nama }}</td>
+                                            <td class="text-center">
+                                                <span>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Change File">
+                                                        <i class="fa fa-refresh color-muted m-r-5"></i>
+                                                    </a>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Open File">
+                                                        <i class="fa fa-eye color-danger"></i>
+                                                    </a>
+                                                </span>
+                                            </td>
+                                            <td class="text-center">
+                                                <span>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit Data">
+                                                        <i class="fa fa-pencil color-muted m-r-5"></i>
+                                                    </a>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete Data">
+                                                        <i class="fa fa-close color-danger"></i>
+                                                    </a>
+                                                </span>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- END CARD2 --}}
+
     </div>
 </div>
+@endsection
+
+@section('add_script')
+<script>
+    function info_pertemuan(bool){
+        var pert_det = document.getElementById('pertemuan-detail');
+        if(bool){
+            pert_det.classList.remove('d-none');
+        }else{
+            pert_det.classList.add('d-none');
+        }
+    }
+    function info_kegiatan(id){
+        var keg = document.getElementsByClassName('check-kegiatan');
+        var file = document.getElementsByClassName('check-file');
+        var kuis = document.getElementsByClassName('check-kuis');
+        for (let index = 0; index < keg.length; index++) {
+            if(index == id){
+                keg[index].classList.remove('d-none');
+                file[index].classList.remove('d-none');
+                kuis[index].classList.remove('d-none');
+            }else{
+                keg[index].classList.remove('d-none');
+                keg[index].classList.add('d-none');
+                file[index].classList.remove('d-none');
+                file[index].classList.add('d-none');
+                kuis[index].classList.remove('d-none');
+                kuis[index].classList.add('d-none');
+            }
+        }
+    }
+</script>
 @endsection
