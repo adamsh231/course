4eb1a3e4ac71e7b53f147a5caa58da14ff8686a1
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-03-14 07:31:35 +0700

    Adding Admin Function for Pertemuan

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,16 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Siswa;
+use App\Pertemuan;
 
 class AdminController extends Controller
 {
-    function administrator(){
+    function administrator()
+    {
         $siswa = $this->getAllSiswa();
-        return view('admin', ['siswa' => $siswa]);
+        $pertemuan = $this->getAllPertemuan();
+        return view(
+            'admin',
+            [
+                'siswa' => $siswa,
+                'pertemuan' => $pertemuan
+            ]
+        );
     }
 
-    function getAllSiswa(){
+    function getAllSiswa()
+    {
         $siswa = Siswa::all();
         return $siswa;
+    }
+
+    function getAllPertemuan()
+    {
+        $pertemuan = Pertemuan::all();
+        return $pertemuan;
     }
 }
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,7 +8,11 @@
             <div class="card-body">
                 <div class="default-tab">
                     <ul class="nav nav-tabs mb-3" role="tablist">
-                        <li class="nav-item"><a class="nav-link active show" data-toggle="tab" href="#siswa">Siswa</a>
+                        <li class="nav-item">
+                            <a class="nav-link active show" data-toggle="tab" href="#siswa">Siswa</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#pertemuan">Pertemuan</a>
                         </li>
                     </ul>
                     <div class="tab-content">
@@ -47,6 +51,63 @@
                                             <td class="text-center">{{ $s->pretest }}</td>
                                             <td class="text-center">{{ $s->posttest }}</td>
                                             <td class="text-center">{{ $s->team }}</td>
+                                            <td class="text-center">
+                                                <span>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit Data">
+                                                        <i class="fa fa-pencil color-muted m-r-5"></i>
+                                                    </a>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete Data">
+                                                        <i class="fa fa-close color-danger"></i>
+                                                    </a>
+                                                </span>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="pertemuan" role="tabpanel">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-striped verticle-middle">
+                                    <thead>
+                                        <tr class="text-center">
+                                            <th scope="col">No</th>
+                                            <th scope="col">Nama</th>
+                                            <th scope="col">Tanggal</th>
+                                            <th scope="col">Diskusi</th>
+                                            <th scope="col">Tugas</th>
+                                            <th scope="col">Deskripsi</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($pertemuan as $p)
+                                        <tr>
+                                            <td class="text-center">{{ $loop->iteration }}</td>
+                                            <td>{{ $p->nama }}</td>
+                                            <td class="text-center">{{ date('d/m/Y', strtotime($p->tanggal)) }}</td>
+                                            <td class="text-center">
+                                                <span>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Change File">
+                                                        <i class="fa fa-refresh color-muted m-r-5"></i>
+                                                    </a>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Open File">
+                                                        <i class="fa fa-eye color-danger"></i>
+                                                    </a>
+                                                </span>
+                                            </td>
+                                            <td class="text-center">
+                                                <span>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Change File">
+                                                        <i class="fa fa-refresh color-muted m-r-5"></i>
+                                                    </a>
+                                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Open File">
+                                                        <i class="fa fa-eye color-danger"></i>
+                                                    </a>
+                                                </span>
+                                            </td>
+                                            <td>{{ $p->deskripsi }}</td>
                                             <td class="text-center">
                                                 <span>
                                                     <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit Data">
