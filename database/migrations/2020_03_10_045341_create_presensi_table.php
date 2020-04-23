238574f87cac5caa0e0969ba84337700e71d3839
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-04-24 01:10:08 +0700

    Reworked Table Presensi + Tugas

--- database/migrations/2020_03_10_045341_create_presensi_table.php
+++ database/migrations/2020_03_10_045341_create_presensi_table.php
@@ -17,6 +17,7 @@ class CreatePresensiTable extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('id_pertemuan');
             $table->unsignedBigInteger('id_siswa');
+            $table->string('tugas')->nullable();
             $table->enum('kehadiran',['Tidak Hadir', 'Hadir'])->default('Tidak Hadir');
             $table->timestamps();
         });
