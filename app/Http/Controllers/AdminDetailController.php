4778c9b93239d3ca1a76b32a200ba7f4267a905a
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-11-28 22:46:40 +0700

    Admin Add Latihan v2

--- app/Http/Controllers/AdminDetailController.php
+++ app/Http/Controllers/AdminDetailController.php
@@ -506,7 +506,7 @@ class AdminDetailController extends Controller
                         <a onclick='fill_edit_latihan($l->id)' href='#' data-toggle='tooltip' data-placement='top' title='' data-original-title='Edit Data'>
                             <i class='fa fa-pencil color-muted m-r-5'></i>
                         </a>
-                        <a onclick='confirm_delete_latihan($s->id, \"$s->pertanyaan\")' href='#' data-toggle='tooltip' data-placement='top' title='' data-original-title='Delete Data'>
+                        <a onclick='confirm_delete_latihan($l->id, \"$l->pertanyaan\")' href='#' data-toggle='tooltip' data-placement='top' title='' data-original-title='Delete Data'>
                             <i class='fa fa-trash-o color-danger'></i>
                         </a>
                     </span>
